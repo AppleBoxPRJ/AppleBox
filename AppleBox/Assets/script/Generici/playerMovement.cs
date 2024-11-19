e8102cbf146cd85479ef8b9e14d8ce11206704e4
Leverdrai <69008499+Leverdrai@users.noreply.github.com>
2024-11-19 23:48:15 +0100

    cambiato IDE raffinato leggermente script

--- AppleBox/Assets/script/Generici/playerMovement.cs
+++ AppleBox/Assets/script/Generici/playerMovement.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -10,35 +7,29 @@ public class playerMovement : MonoBehaviour
     public float speed = 12f;
     public float gravity = -9.81f;
     public Vector3 velocity;
-    public Controls moveAction;
     public Vector2 movementDirection;
     public Transform cameraTransform;
     public Vector3 forward;
     public Vector3 right;
     // Start is called before the first frame update
-    void Awake()
-    {
-
-    }
-
     public void OnMove(InputAction.CallbackContext context)
     {
         movementDirection = context.ReadValue<Vector2>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         Movements();
     }
 
-    void Movements()
+    private void Movements()
     {        
         forward = cameraTransform.forward;
         right = cameraTransform.right;
         Vector3 move = (forward * movementDirection.y + right * movementDirection.x); //new Vector3(-movementDirection.y, 0, -movementDirection.x) * speed * Time.deltaTime;
-        Debug.Log($"Move Input: {movementDirection}");
-        controller.Move(move * speed *Time.deltaTime);
+        //Debug.Log($"Move Input: {movementDirection}");
+        controller.Move(move * speed * Time.deltaTime);
 
         //gestione della gravita'
         velocity.y += gravity * Time.deltaTime;
