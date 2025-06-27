36f57eaf7e88446f336570dee4e489731f7215dc
Stefano Giordano <ste.giordano02@gmail.com>
2025-06-27 18:09:19 +0200

    pause menu fix

--- Assets/_Script/Generici/InputBindings/InputBindings.cs
+++ Assets/_Script/Generici/InputBindings/InputBindings.cs
@@ -5,13 +5,23 @@ public class InputBindings : MonoBehaviour
 {
     [SerializeField] private InputActionAsset _inputActionAsset;
 
+    // Player Actions
     public InputAction MoveAction;
+    public InputAction LookAction;
     public InputAction InteractAction;
+    
+    // UI Actions
+    public InputAction SubmitAction;
+    public InputAction CancelAction;
 
     void Awake()
     {
         MoveAction = _inputActionAsset.FindAction("Move");
+        LookAction = _inputActionAsset.FindAction("Look");
         InteractAction = _inputActionAsset.FindAction("Interact");
+        
+        SubmitAction  = _inputActionAsset.FindAction("Submit");
+        CancelAction = _inputActionAsset.FindAction("Cancel");
         
         _inputActionAsset.Enable();
         
--- Assets/_Script/UI/StartingMessage.cs
+++ Assets/_Script/UI/StartingMessage.cs
@@ -1,6 +1,6 @@
-using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
+
 public class StartingMessage : MonoBehaviour
 {
     void OnEnable()
@@ -8,13 +8,9 @@ public class StartingMessage : MonoBehaviour
         InputBindings.Instance.InteractAction.performed += Destroy;
     }
 
-    void OnDisable()
-    {
-        InputBindings.Instance.InteractAction.performed  -= Destroy;
-    }
-
     private void Destroy(InputAction.CallbackContext ctx)
     {
         Destroy(gameObject);
+        InputBindings.Instance.InteractAction.performed  -= Destroy;
     }
 }
