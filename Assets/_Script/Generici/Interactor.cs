0dc530b0c72e16fa24486bca4b1590c6597e9051
Leverdrai <69008499+Leverdrai@users.noreply.github.com>
2025-07-01 16:08:03 +0200

    implemented Change cursor when over interactable

--- Assets/_Script/Generici/Interactor.cs
+++ Assets/_Script/Generici/Interactor.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 interface IInteractable
 {
@@ -9,29 +10,42 @@ interface IInteractable
 public class Interactor : MonoBehaviour
 {
     public Transform InteractionSource;
-    
-    private float _interactionRange = 3f;
+    [SerializeField] private float _interactionRange = 3f;
+
+    [Header("icons")] 
+    [SerializeField] private Image _crosshair;
+    [SerializeField] private Image _hand;
+
+    private Ray r;
 
     private void OnEnable()
     {
         InputBindings.Instance.InteractAction.performed += ShootRaycast;
+        _hand.gameObject.SetActive(false);
+        
     }
 
-    // void Update()
-    // {
-    //     if(TestoDaScomparire.playerInTrigger)
-    //     {
-    //         _interactionRange = 0f;
-    //     }
-    // }
+    void Update()
+    {
+        r = new Ray(InteractionSource.position, InteractionSource.forward);
+        if (Physics.Raycast(r, out RaycastHit hitInfo, _interactionRange) && hitInfo.collider.gameObject.TryGetComponent(out IInteractable interactionObject))
+        {
+            _hand.gameObject.SetActive(true);
+            _crosshair.gameObject.SetActive(false);
+        }
+        else
+        {
+            _hand.gameObject.SetActive(false);
+            _crosshair.gameObject.SetActive(true);
+        }
+    }
 
     private void ShootRaycast(InputAction.CallbackContext ctx)
     {
         // Crea un ray che viaggia in avanti fino al massimo range
-        var r = new Ray(InteractionSource.position, InteractionSource.forward);
             
         if (Physics.Raycast(r, out RaycastHit hitInfo, _interactionRange))
-        {
+        {                
             // Se collide, prova ad interagire con quello con cui ha colliso
             if (hitInfo.collider.gameObject.TryGetComponent(out IInteractable interactionObject))
             {
